b58e1fbb5d7bf260cfee6be76f6e796774db56f3
Ashton <thejagar22@gmail.com>
2026-09-23 03:37:26 -0600

    Test commit to make sure all is working still.

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -11,6 +11,7 @@ let adminUserID;
 let adminUserName;
 let franchiseID; 
 
+
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
